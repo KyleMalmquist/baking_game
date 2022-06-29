196343ca9ad6b3c0736077db7e8a84f4c444afdc
Kyle <hello@kylemalmquist.com>
2022-06-28 22:40:47 -0500

    added basic graphics and map generation

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ class Game:
         self.screen = screen
         self.objects = []
         self.game_state = GameState.NONE
+        self.map = []
 
     def set_up(self):
         player = Player(1, 1)  # position the player starts at
@@ -17,10 +18,14 @@ class Game:
         print("do set-up")
         self.game_state = GameState.RUNNING
 
+        self.load_map("01")
+
     def update(self):
         self.screen.fill(config.BLACK)
         print("update")
         self.handle_events()
+
+        self.render_map(self.screen)
 
         for object in self.objects:  # objects must have a render class to be properly rendered
             object.render(self.screen)
@@ -35,7 +40,7 @@ class Game:
                 if event.key == pygame.K_ESCAPE:
                     self.game_state = GameState.ENDED
 
-                    # moves the player using WASD
+                # moves the player using WASD
                 elif event.key == pygame.K_w:
                     self.player.update_position(0, -1)
                 elif event.key == pygame.K_a:
@@ -45,3 +50,31 @@ class Game:
                 elif event.key == pygame.K_d:
                     self.player.update_position(1, 0)
 
+    def load_map(self, file_name):
+        with open('maps/' + file_name + ".txt") as map_file:
+            for line in map_file:
+                tiles = []
+                for i in range(0, len(line), 2,):  # -1 to account for blank line at the end of the map file
+                    tiles.append(line[i])
+
+                self.map.append(tiles)
+
+            print(self.map)
+
+    def render_map(self, screen):
+        y_position = 0
+        for line in self.map:
+            x_position = 0
+            for tile in line:
+                image = map_tile_image[tile]
+                rect = pygame.Rect(x_position * config.SCALE, y_position * config.SCALE, config.SCALE, config.SCALE)
+                screen.blit(image, rect)
+                x_position = x_position + 1
+
+            y_position = y_position + 1
+
+
+map_tile_image = {
+    "G" : pygame.transform.scale(pygame.image.load('imgs/grass1.png'), (config.SCALE, config.SCALE)),
+    "W" : pygame.transform.scale(pygame.image.load('imgs/water.png'), (config.SCALE, config.SCALE))
+}
